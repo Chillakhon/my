23a1a899d799ae97423e3cdff539fe04e633e5b8
chollakhon <shoev-03@mail.ru>
2023-10-03 10:18:39 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once "vendor/autoload.php";
+
+use App\Data\User;
+Use App\Data\Books\Books;
+Use App\Data\Books\Miss;
+
+
+$user = new User();
+$book = new Books();
+$miss = new Miss();
+
---
+++ src/Data/Books/Books.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Data\Books;
+
+Class Books
+{
+    public function test()
+    {
+        echo 'Hello World';
+    }
+}
